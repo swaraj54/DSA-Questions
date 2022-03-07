f87ed8b7a4249644b4e5d9519c734b389e3c3967
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2022-03-07 23:48:53 +0530

    update

--- A_Code_Snipest.js
+++ A_Code_Snipest.js
@@ -1,11 +1,12 @@
 (function main() {
+   
     var solution = function(arr) {
       
        return arr;       
        
     };
    
-    
+
     var arr = [3,5,3,9,2,5,2];
     console.log(solution(arr));
   })();
