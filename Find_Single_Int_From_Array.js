b0c688ef64e15ca5a950747d057e5e26413976fe
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2022-03-08 22:40:47 +0530

    update

--- Find_Single_Int_From_Array.js
+++ Find_Single_Int_From_Array.js
@@ -1,23 +1,19 @@
 (function main() {
 
-    
-
     var solution = function(arr) {
-        
-        for (var i =0; i< arr.length; i++){
-            var counter = 0;
-            for (var j = i+1; j < arr.length-1; j++){
-                if(arr[i] === arr[j]){
-                    counter++;
+        console.log(arr)
+        for (var i = 0; i < arr.length; i++){
+            var flag = true;
+            for (var j = i+1; j < arr.length; j++){
+                if( arr[i]===arr[j] ){
+                    flag = false;
                 }
-
             }
-            if(counter === 0){
-                console.log(arr[i]);
+            if(flag === true ){
+                console.log(arr[i])
+                break;
             }
-
         }      
-       
     };
    
     var arr = [3,5,3,9,2,5,2];
